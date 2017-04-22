ab96b487cd0c58b9f0e5e8fe10e6acf6870672e8
Edward Teddy Brow <edward.teddy.brow@gmail.com>
2017-04-21 21:48:51 -0700

    suppress makeall.py's output by default

--- makeall.py
+++ makeall.py
@@ -4,18 +4,22 @@ import argparse
 import os
 import re
 import subprocess
+import sys
 
 parser = argparse.ArgumentParser(description='Run `make (clean)` for all exercises.')
 parser.add_argument('-c', '--clean', action='store_true', help='Run `make clean` rather than just `make`.')
+parser.add_argument('-v', '--verbose', action='store_true', help='Show all output.')
 args = parser.parse_args()
 
 subdirs = [f for f in os.listdir('.') if re.match(r'[0-9][{2}_[0-9]{2}', f)]
+out = sys.stdout if args.verbose else open(os.devnull, 'w')
+err = sys.stderr if args.verbose else open(os.devnull, 'w')
 
 for subdir in subdirs:
     if 'Makefile' in os.listdir(subdir):
         os.chdir(subdir)
         if args.clean:
-            subprocess.call(['make', 'clean'])
+            subprocess.call(['make', 'clean'], stdout=out, stderr=err)
         else:
-            subprocess.call(['make'])
+            subprocess.call(['make'], stdout=out, stderr=err)
         os.chdir('..')
